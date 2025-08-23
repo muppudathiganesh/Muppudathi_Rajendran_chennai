ee61f8a69e03e03d9f94dafe4fefe2129ac3e399
muppudathiganesh <muppuraj11@gmail.com>
2025-08-23 18:29:25 +0530

    Resolved merge conflict in models.py

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 from django.db import models
 
 # Create your models here.
-=======
-from django.db import models
 
-# Create your models here.
->>>>>>> 9283bda8974f6b64c7b05e78e21b2fca59cb27fe
