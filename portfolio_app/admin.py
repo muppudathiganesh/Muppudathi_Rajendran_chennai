d3d1fbc7e710901f6670bfbbc040ba3a3e0c4efe
muppudathiganesh <muppuraj11@gmail.com>
2025-08-23 18:23:17 +0530

    Resolved merge conflict in admin.py

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
 
-# Register your models here.
->>>>>>> 9283bda8974f6b64c7b05e78e21b2fca59cb27fe
