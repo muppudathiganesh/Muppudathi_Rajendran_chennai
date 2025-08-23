bd6078f4c98628eaed231a1bed38fafc9f7e8c73
muppudathiganesh <muppuraj11@gmail.com>
2025-08-23 18:30:18 +0530

    Resolved merge conflict in urls.py

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
      path('success/', views.success_page, name='thankyou'),
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-     path('success/', views.success_page, name='thankyou'),
->>>>>>> 9283bda8974f6b64c7b05e78e21b2fca59cb27fe
 ]
