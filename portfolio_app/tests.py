390a6700f63493ef9f827a46ad3a1640407cc4b3
muppudathiganesh <muppuraj11@gmail.com>
2025-08-23 18:29:58 +0530

    Resolved merge conflict in tests.py

--- portfolio_app/tests.py
+++ portfolio_app/tests.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 from django.test import TestCase
 
-# Create your tests here.
-=======
-from django.test import TestCase
 
-# Create your tests here.
->>>>>>> 9283bda8974f6b64c7b05e78e21b2fca59cb27fe
