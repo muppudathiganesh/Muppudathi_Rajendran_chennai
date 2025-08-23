27a6cb0ae4622fa228fb9bda8b96c1a460a6dfcf
muppudathiganesh <muppuraj11@gmail.com>
2025-08-23 18:32:13 +0530

    Resolved merge conflict in asgi.py

--- portfolio_project/asgi.py
+++ portfolio_project/asgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 ASGI config for portfolio_project project.
 
@@ -15,21 +15,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for portfolio_project project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
-
-application = get_asgi_application()
->>>>>>> 9283bda8974f6b64c7b05e78e21b2fca59cb27fe
