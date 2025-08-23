b23a54ac082775a4b674845589070e6dcb82d56d
muppudathiganesh <muppuraj11@gmail.com>
2025-08-23 18:43:46 +0530

    Update settings.py for deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR /  "portfolio_app" / "static"]  # if you have a global static folder
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
